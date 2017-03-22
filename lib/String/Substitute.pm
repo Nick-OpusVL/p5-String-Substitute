5fb331eeab7b4857d05a52742fd646b86ba24043
Nick Booker <nmb@nickbooker.uk>
2017-03-22 11:58:46 +0000

    Add support and copyright info

--- lib/String/Substitute.pm
+++ lib/String/Substitute.pm
@@ -83,4 +83,21 @@ As a one-liner it might look like this:
 
     perl -Ilib -MString::Substitute=get_all_substitutes -E 'say for get_all_substitutes(string => "ABC", substitutions => { A => "Aa", B => "Bb" })'
 
+=head1 SUPPORT
+
+If you require assistance, support, or further development of this software, please contact OpusVL using the details below:
+
+Telephone: +44 (0)1788 298 410
+
+Email: community@opusvl.com
+
+Web: http://opusvl.com
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright (C) 2017 Opus Vision Limited
+
+This is free software; you can redistribute it and/or modify it under the
+same terms as the Perl 5 programming language system itself.
+
 =cut
